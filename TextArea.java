71bdcda0dcea833f73efddf2e6ce112a7a3e2afc
diegoLopez <diplez@hotmail.com>
2020-05-22 15:48:03 -0500

    Correcion para clase main

--- TextArea.java
+++ TextArea.java
@@ -3,24 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package JOptionPane;
+
 
 /**
  *
  * @author Asus
  */
-import java.awt.Color;
-import java.awt.Dimension;
-
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.SpringLayout;
 
 public class TextArea {
 
